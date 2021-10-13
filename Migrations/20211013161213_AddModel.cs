d6f73125e2404f481a29db018ccac0d3ea98d9b8
IleaneBB <a2020110849@estudiantes.upsa.edu.bo>
2021-10-13 13:07:50 -0400

    Agregar archivos de proyecto.

---
+++ Migrations/20211013161213_AddModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CancionesWebAPI.Prueba.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cancion",
+                columns: table => new
+                {
+                    CancionID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Lyrics = table.Column<string>(type: "nvarchar(max)", maxLength: 90000, nullable: true),
+                    URL = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cancion", x => x.CancionID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cancion");
+        }
+    }
+}
---
+++ Models/Cancion.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CancionesWebAPI.Prueba.Models
+{
+    public class Cancion
+    {
+        [Key]
+        public string CancionID { get; set; }
+        [Display(Name = "Autor Principal")]
+        public string Author { get; set; }
+        [StringLength(90000, MinimumLength = 2, ErrorMessage = "INGRESE ENTRE 2 A 90000 CARACTERES")]
+        [Display(Name = "Letra de la canción")]
+        public string Lyrics { get; set; }
+        [Display(Name = "Enlace de youtube")]
+        public string URL { get; set; }
+    }
+}
